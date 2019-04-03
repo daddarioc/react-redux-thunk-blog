c5b807dfe68301bfcadd8f9c74138a7a059da0b0
daddaric <christopher.daddario@espn.com>
2019-04-03 15:27:16 -0400

    final working version

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,36 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    //pulls off unique userIds from response into an array
+    /* const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id))); */
+
+    //a refactor of the above using _chain from lodash similar to 
+    //using collection functions with lambdas in Java
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
     dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+};
+
+// memoized version of preventing over-fetching
+/* export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+}); */
+   
